03bc45bb87e866b6b6ec45a5072369bc653f2a9b
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-07-12 23:36:15 -0700

    check if tile is even or add, and adds color dependent on answer

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -4,18 +4,28 @@ import './chessboard.css';
     //* horizontal tiles
 const hAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
     //* vertical tiles
-const vAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const vAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 export default function chessboard() {
     //* creates an array
         let board = [];
-    //* loops through horizontal
-        for(let i = 0; i < hAxis.length; i++) {
-        //* invert loops through vertical axis
-            for(let j = vAxis.length-1; j >= 0; j--) {
+
+    //* invert loops through vertical axis
+        for(let j = vAxis.length-1; j >= 0; j--) {
+        //* loops through horizontal
+            for(let i = 0; i < hAxis.length; i++) {
+                const number = j + i + 2;
+
+                if(number % 2 === 0) {
+                    board.push(
+                        <div className="tile black-tile">{hAxis[i]}-{vAxis[j]}</div>);
+                } else {
+                    board.push(
+                        <div className="tile white-tile">{hAxis[i]}-{vAxis[j]}</div>);
+                }
                 console.log(hAxis[i], vAxis[j]);
             //* combines V and H axis' and puts it on the board
-                board.push(<span>{hAxis[i]}-{vAxis[j]}</span>);
+
             }
         }
     return <div id="chessboard">{board}</div>;
