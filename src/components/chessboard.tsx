588b705b5dedbb400e9090b5ca501760ef494f7e
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-07-12 22:38:38 -0700

    added for loops to name grid

--- src/components/chessboard.tsx
+++ src/components/chessboard.tsx
@@ -7,5 +7,16 @@ const hAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const vAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 
 export default function chessboard() {
-    return <div id="chessboard"></div>;
+    //* creates an array
+        let board = [];
+    //* loops through horizontal
+        for(let i = 0; i < hAxis.length; i++) {
+        //* invert loops through vertical axis
+            for(let j = vAxis.length-1; j >= 0; j--) {
+                console.log(hAxis[i], vAxis[j]);
+            //* combines V and H axis' and puts it on the board
+                board.push(<span>{hAxis[i]}-{vAxis[j]}</span>);
+            }
+        }
+    return <div id="chessboard">{board}</div>;
 }
