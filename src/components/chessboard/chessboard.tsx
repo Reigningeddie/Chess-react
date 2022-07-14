d4d96ea4216dc09eed656e51cfb7ec41d8d316b2
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-07-14 00:07:47 -0700

    inserted pawns starting positions

--- src/components/chessboard/chessboard.tsx
+++ src/components/chessboard/chessboard.tsx
@@ -6,6 +6,27 @@ const hAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
     //* vertical tiles
 const vAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
+    //* creates a piece interface
+interface Piece {
+    image: string;
+    x: number;
+    y: number;
+}
+
+    //* creates a piece array
+const pieces: Piece[] = [];
+
+    //* loops through numbers 1-8 for black pawns
+for(let i = 0; i < 8; i++) {
+//* hard codes black pawns into selected tile array
+    pieces.push({ image: "assets/images/Pawn_B.png", x: i, y: 6 })
+};
+
+    //* loops through numbers 1-8 for white pawns
+for(let i = 0; i < 8; i++) {
+//* hard codes white pawns into selected tile array
+    pieces.push({ image: "assets/images/Pawn_W.png", x: i, y: 1 })
+}
 export default function chessboard() {
     //* creates an array
         let board = [];
@@ -16,11 +37,18 @@ export default function chessboard() {
             for(let i = 0; i < hAxis.length; i++) {
             //* adds both axis arrays and adds 1 because arrays start at 0
                 const number = j + i + 2;
-            //* takes the number and adds it to the tile - logic continued in tile.tsx
-                board.push(<Tile number={ number } />);
+                let image = undefined;
 
-                console.log(hAxis[i], vAxis[j]);
-            //* combines V and H axis' and puts it on the board
+                pieces.forEach(p => {
+                    if(p.x === i && p.y === j) {
+                        image = p.image;
+                    }
+                })
+
+            //* takes the number and adds it to the tile - logic continued in tile.tsx
+                board.push(<Tile image={image} number={number} />);
+
+
 
             }
         }
