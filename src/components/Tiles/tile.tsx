265b3dc114b45aa93ca49c3e83a1a86105876a47
Eduardo Aguilar <eaguilar92105@yahoo.com>
2022-07-13 23:29:41 -0700

    moved assets folder and created an image props

--- src/components/Tiles/tile.tsx
+++ src/components/Tiles/tile.tsx
@@ -1,16 +1,23 @@
 import './tile.css';
 
 interface Props {
+    image?: string;
     number: number
 }
 
-export default function tile({ number }: Props) {
+export default function tile({ number, image }: Props) {
 //* checks if number is divisible by 2
     if (number % 2 === 0) {
     //* if not divisible by 2 then turn tile black
-        return <div className="tile black-tile"></div>;
+        return (
+        <div className="tile black-tile">
+            <img src={image} alt=""/>
+        </div>);
     } else {
     //* if divisible by 2 then turn tile white
-        return <div className="tile white-tile"></div>;
+        return (
+        <div className="tile white-tile">
+            <img src={image} alt="" />
+        </div>);
     }
 }
